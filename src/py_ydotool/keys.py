fdc43ed943bbbd0cbc5dac2965cf0ac5cca05128
kyaoi <aoistudy90@gmail.com>
2026-03-28 15:22:31 +0900

    feat: expand key constants and add key tests

--- src/py_ydotool/keys.py
+++ src/py_ydotool/keys.py
@@ -1,24 +1,98 @@
 class Key:
-    ENTER = 28
     ESC = 1
-    TAB = 15
-    SPACE = 57
+
+    NUM_1 = 2
+    NUM_2 = 3
+    NUM_3 = 4
+    NUM_4 = 5
+    NUM_5 = 6
+    NUM_6 = 7
+    NUM_7 = 8
+    NUM_8 = 9
+    NUM_9 = 10
+    NUM_0 = 11
+
+    MINUS = 12
+    EQUAL = 13
     BACKSPACE = 14
-    DELETE = 111
+    TAB = 15
 
-    CTRL = 29
-    SHIFT = 42
-    ALT = 56
-    META = 125
-
-    UP = 103
-    DOWN = 108
-    LEFT = 105
-    RIGHT = 106
+    Q = 16
+    W = 17
+    E = 18
+    R = 19
+    T = 20
+    Y = 21
+    U = 22
+    I = 23  # noqa: E741
+    O = 24  # noqa: E741
+    P = 25
+    LEFTBRACE = 26
+    RIGHTBRACE = 27
+    ENTER = 28
+    LEFT_CTRL = 29
 
     A = 30
-    C = 46
+    S = 31
+    D = 32
+    F = 33
+    G = 34
+    H = 35
     J = 36
+    K = 37
     L = 38
-    T = 20
+    SEMICOLON = 39
+    APOSTROPHE = 40
+    GRAVE = 41
+    LEFT_SHIFT = 42
+    BACKSLASH = 43
+
+    Z = 44
+    X = 45
+    C = 46
     V = 47
+    B = 48
+    N = 49
+    M = 50
+    COMMA = 51
+    DOT = 52
+    SLASH = 53
+    RIGHT_SHIFT = 54
+    LEFT_ALT = 56
+    SPACE = 57
+    CAPSLOCK = 58
+
+    F1 = 59
+    F2 = 60
+    F3 = 61
+    F4 = 62
+    F5 = 63
+    F6 = 64
+    F7 = 65
+    F8 = 66
+    F9 = 67
+    F10 = 68
+    NUMLOCK = 69
+    SCROLLLOCK = 70
+    F11 = 87
+    F12 = 88
+
+    RIGHT_CTRL = 97
+    RIGHT_ALT = 100
+    HOME = 102
+    UP = 103
+    PAGEUP = 104
+    LEFT = 105
+    RIGHT = 106
+    END = 107
+    DOWN = 108
+    PAGEDOWN = 109
+    INSERT = 110
+    DELETE = 111
+    LEFT_META = 125
+    RIGHT_META = 126
+
+    CTRL = LEFT_CTRL
+    SHIFT = LEFT_SHIFT
+    ALT = LEFT_ALT
+    META = LEFT_META
